c32aa2fd9b5a54acc00c93c022999a710ae2de40
debabrata-97 <debabrataszr@gmail.com>
2023-03-09 19:02:53 +0530

    Added: Code to print the maximum value of Integer

--- src/MaximumValue.java
+++ src/MaximumValue.java
@@ -1,30 +1,14 @@
-public class MaximumValue<T extends Comparable<T>> {
-    public static <T extends Comparable<T>> T maximum(T... number) {
-        T maximumValue = number[0];
-        for (int i = 1; i < number.length; i++) {
-            if (number[i].compareTo(maximumValue) > 0) {
-                maximumValue = number[i];
-            }
-        }
-        System.out.println("given values are : ");
-        for (int i = 0; i < number.length; i++) {
-            System.out.print(number[i] + " ");
-        }
-        System.out.println();
-        printMax(maximumValue);
+public class MaximumValue {
+    public Integer maximumInteger(Integer a, Integer b, Integer c) {
+        Integer maximumValue = a;
+        if (b.compareTo(maximumValue) > 0)
+            maximumValue = b;
+        if (c.compareTo(maximumValue) > 0)
+            maximumValue = c;
         return maximumValue;
     }
-
-    public static <T> void printMax(T maximum) {
-        System.out.println("Maximum Value is : " + maximum);
+    public static void main(String[] args){
+        MaximumValue maxValue=new MaximumValue();
+        System.out.println("Maximum Value of Integer : "+ maxValue.maximumInteger(20,10,25));
     }
-
-    // main method
-    public static void main(String[] args) {
-        maximum(28, 78, 38, 68, 98);
-        maximum("xyz", "def", "jkl", "dog", "war");
-        maximum(27f, 9f, 4.7f, 57.3f, 34.9f);
-
-    }
-
 }
