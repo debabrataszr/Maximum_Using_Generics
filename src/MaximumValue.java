a151a14c82cb4be39d0c13db29a1ca7973389411
debabrata-97 <debabrataszr@gmail.com>
2023-03-09 20:06:04 +0530

    Added: Code to Print in sorting order

--- src/MaximumValue.java
+++ src/MaximumValue.java
@@ -1,23 +1,31 @@
-public class MaximumValue<T extends Comparable<T>> {
-    T num1;
-    T num2;
-    T num3;
+import java.util.Arrays;
 
-    public static <T extends Comparable<T>> T maximumGeneric(T num1, T num2, T num3) {
-        T maximumValue = num1;
-        if (num2.compareTo(maximumValue) > 0)
-            maximumValue = num2;
-        if (num3.compareTo(maximumValue) > 0)
-            maximumValue = num3;
-        System.out.println(num1 + " " + num2 + " " + num3);
+public class MaximumValue<T extends Comparable<T>> {
+    T[] maximumValue;
+
+    public MaximumValue(T[] maximumValue) {
+        this.maximumValue = maximumValue;
+
+    }
+
+    public static <T extends Comparable<T>> T[] maximum(T... maximumValue) {
+        System.out.println("Given elements -> ");
+        for (T element : maximumValue) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+        System.out.println("After Sorting -> ");
+        Arrays.sort(maximumValue);
+        for (T element : maximumValue) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
         return maximumValue;
     }
 
     public static void main(String[] args) {
-        MaximumValue maximumValue = new MaximumValue();
-        System.out.println("Maximum Integer value : " + maximumGeneric(10, 34, 28));
-        System.out.println("Maximum Float value : " + maximumGeneric(25.8f, 40.9f, 57.2f));
-        System.out.println("Maximum String value : " + maximumGeneric("xyz", "dew", "res"));
-
+        maximum(10, 34, 85, 28, 45);
+        maximum("xyz", "dew", "jkl", "res", "abc");
+        maximum(25.8f, 40.9f, 57.2f, 12.3f, 5.4f);
     }
 }
