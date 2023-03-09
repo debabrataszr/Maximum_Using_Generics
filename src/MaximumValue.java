4163cfda770defcdc14d5617f42fc4eeeafd4237
debabrata-97 <debabrataszr@gmail.com>
2023-03-09 19:11:07 +0530

    Added: Code to print the maximum value of String

--- src/MaximumValue.java
+++ src/MaximumValue.java
@@ -1,15 +1,16 @@
 public class MaximumValue {
-    public Float maximumFloat(Float a, Float b, Float c) {
-        Float maximumValue = a;
+    public static String maximumString(String a, String b, String c) {
+        String maximumValue = a;
         if (b.compareTo(maximumValue) > 0)
             maximumValue = b;
         if (c.compareTo(maximumValue) > 0)
             maximumValue = c;
+        System.out.println(a + " " + b + " " + c);
         return maximumValue;
-
     }
-    public static void main(String[] args){
-        MaximumValue maximumValue=new MaximumValue();
-        System.out.println(maximumValue.maximumFloat(30.50F,15.05F,19.58F));
+
+    public static void main(String[] args) {
+        MaximumValue maximumValue = new MaximumValue();
+        System.out.println("Maximum value of String : " + maximumValue.maximumString("grapes", "mango", "orange"));
     }
 }
