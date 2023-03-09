922849d16d3cb3b5a8ea52f5a08957cda9b28132
debabrata-97 <debabrataszr@gmail.com>
2023-03-09 19:51:36 +0530

    Added: Code to Print the Maximum value of Generic Type

--- src/MaximumValue.java
+++ src/MaximumValue.java
@@ -3,12 +3,6 @@ public class MaximumValue<T extends Comparable<T>> {
     T num2;
     T num3;
 
-    public MaximumValue(T num1, T num2, T num3) {
-        this.num1 = num1;
-        this.num2 = num2;
-        this.num3 = num3;
-    }
-
     public static <T extends Comparable<T>> T maximumGeneric(T num1, T num2, T num3) {
         T maximumValue = num1;
         if (num2.compareTo(maximumValue) > 0)
@@ -20,12 +14,10 @@ public class MaximumValue<T extends Comparable<T>> {
     }
 
     public static void main(String[] args) {
-        Integer intValue = maximumGeneric(10, 34, 28);
-        Float floatValue = maximumGeneric(25.8f, 40.9f, 57.2f);
-        String stringValue = maximumGeneric("xyz", "dew", "res");
-        System.out.println("Maximum Integer value : " + intValue);
-        System.out.println("Maximum Float value : " + floatValue);
-        System.out.println("Maximum String value : " + stringValue);
+        MaximumValue maximumValue = new MaximumValue();
+        System.out.println("Maximum Integer value : " + maximumGeneric(10, 34, 28));
+        System.out.println("Maximum Float value : " + maximumGeneric(25.8f, 40.9f, 57.2f));
+        System.out.println("Maximum String value : " + maximumGeneric("xyz", "dew", "res"));
 
     }
 }
